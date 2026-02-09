6dde0798edd8046fd9669b27faf70c29faf576f1
Marcin Rogulski <Marcinos.Rogulski@gmail.com>
2026-02-09 23:15:12 +0100

    dodanie komponenty player z dzialajacym inputem

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-
+import Player from "./components/Player"
 function App() {
   
 
   return (
-    <header>
-      <p>Coming soon</p>
-    </header>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name='Player 1' symbol='X'/>
+          <Player name='Player 2' symbol='0'/>
+          
+        </ol>
+      </div>
+    </main>
   )
 }
 
