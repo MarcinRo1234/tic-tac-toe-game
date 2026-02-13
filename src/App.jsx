2823f325764b1c094297a23735e9aed4c036937a
Marcin Rogulski <Marcinos.Rogulski@gmail.com>
2026-02-13 23:06:14 +0100

    DOdanie game Board 81

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./components/Player"
+import GameBoard from "./components/GameBoard"
 function App() {
   
 
@@ -8,8 +9,8 @@ function App() {
         <ol id="players">
           <Player name='Player 1' symbol='X'/>
           <Player name='Player 2' symbol='0'/>
-          
         </ol>
+        <GameBoard /> 
       </div>
     </main>
   )
