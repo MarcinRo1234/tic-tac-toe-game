b0dabea318b852a1873bfb0eb9edd45970aa50de
Marcin Rogulski <Marcinos.Rogulski@gmail.com>
2026-02-21 23:32:26 +0100

    dodanie zmian symboli na tablicy nr 83

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
+import { useState } from "react"
 import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
 function App() {
-  
-
+  const [activePlayer, setActivePlayer] = useState('X')
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X')
+  }
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player name='Player 1' symbol='X'/>
-          <Player name='Player 2' symbol='0'/>
+        <ol id="players" className="higlight-player">
+          <Player name='Player 1' symbol='X' isActive={activePlayer === 'X'}/>
+          <Player name='Player 2' symbol='0'isActive={activePlayer === 'O'}/>
         </ol>
-        <GameBoard /> 
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/> 
       </div>
     </main>
   )
